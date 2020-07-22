5fcea4986da4738f62b388e459cf997cf6e73e85
toricruzmendiola <mtoricruz@gmail.com>
2020-07-22 12:04:59 -0700

    completed single_number.py first pass

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,21 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
+# U - we have an array where every element shows up twice except 1, return that 1 element. Loop through the array. Check to see if that element is in current array. If there is element that doesn't show up twice, return that element
 
-    pass
+def single_number(arr):
+    list_1 = []
+    # Loop through array
+    for elem in arr:
+        if elem in list_1:
+            # print(list_1, 'is in list')
+            list_1.remove(elem)
+        else:
+            # print(list_1, 'is not in list')
+            list_1.append(elem)
+    return list_1.pop()
+
+    
 
 
 if __name__ == '__main__':
