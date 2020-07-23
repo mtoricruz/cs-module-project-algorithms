c92ec8071eea52cc58a5108586d79a13cbbdfe39
toricruzmendiola <mtoricruz@gmail.com>
2020-07-23 10:01:44 -0700

    psuedo code added to single_number for future understanding

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -5,15 +5,17 @@ Returns: an integer
 # U - we have an array where every element shows up twice except 1, return that 1 element. Loop through the array. Check to see if that element is in current array. If there is element that doesn't show up twice, return that element
 
 def single_number(arr):
+    # create array to store odd number
     list_1 = []
     # Loop through array
     for elem in arr:
+        # check if elem is in list_1, if so remove it
         if elem in list_1:
-            # print(list_1, 'is in list')
             list_1.remove(elem)
+        # if elem is not in list_1, add to list
         else:
-            # print(list_1, 'is not in list')
             list_1.append(elem)
+    # after full loop, pop number in list and return that value
     return list_1.pop()
 
     
