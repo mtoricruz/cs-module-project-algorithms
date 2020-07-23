97f66cce47cc85f744d1a2347f099a19a61a1f3e
toricruzmendiola <mtoricruz@gmail.com>
2020-07-23 10:07:09 -0700

    psuedo code for moving_zeroes added

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,13 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
+    # loop through length of array-1 
     for elem in range(len(arr)-1):
+        # for every index in the length of array
         for i in range(len(arr)-1):
-            if arr[i]== 0:
+            # check if the index value is 0:
+            if arr[i] == 0:
+                # if it is, swap the integers
                 arr[i+1], arr[i] = arr[i], arr[i+1]
     return arr
 
